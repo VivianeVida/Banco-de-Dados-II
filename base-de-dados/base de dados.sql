b13535794141aa34ca1874600c5975447dc8f54d
Viviane Vida <67081266+VivianeVida@users.noreply.github.com>
2024-09-24 18:43:17 -0300

    Add files via upload

---
+++ base-de-dados/base de dados.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE MeuBancoDeDados;
+USE MeuBancoDeDados;
---
+++ base-de-dados/tabela dados.sql
@@ -0,0 +1,7 @@
+INSERT INTO Clientes (ClienteID, Nome, Email) VALUES
+(1, 'João Silva', 'joao.silva@example.com'),
+(2, 'Maria Oliveira', 'maria.oliveira@example.com');
+
+INSERT INTO Pedidos (PedidoID, DataPedido, ClienteID) VALUES
+(1, '2024-09-01', 1),
+(2, '2024-09-02', 2);
